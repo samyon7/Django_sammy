9ca52cd727f4d0ca6f6d2142ed90edabdd73e072
samyon7 <sammyon7@gmail.com>
2022-05-13 09:42:06 +0700

    Project 2

---
+++ PART_1/project_1-2/bookr/reviews/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+	return render(request,"base.html")
+	
+def book_search(request):
+	search_text = request.GET.get("search", "")
+	return render(request, "base.html", {"search_text":search_text})
